077525fe531a95b597e74ee6b1bfbb0c385a504f
PhongDapChai2003 <168374593+PhongDapChai2003@users.noreply.github.com>
2026-08-16 16:56:16 -0700

    Group firmware under software

---
+++ software/firmware/pico_main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+
+    Serial.println("Pico 2 is running");
+}
